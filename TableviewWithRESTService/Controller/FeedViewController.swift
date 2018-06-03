a2deb198b9a6cbc7a2686384c1bc5b846de76e69
Nischal Hada <hadanischal@gmail.com>
2018-06-03 18:35:11 +1000

    Completed TableView Setup

--- TableviewWithRESTService/Controller/FeedViewController.swift
+++ TableviewWithRESTService/Controller/FeedViewController.swift
@@ -1,35 +1,56 @@
 //
-//  FeedViewController.swift
-//  TableviewWithRESTService
+//  FeedsViewController.swift
+//  iOSProgressiveExercise
 //
-//  Created by Nischal Hada on 5/31/18.
+//  Created by Nischal Hada on 5/29/18.
 //  Copyright © 2018 NischalHada. All rights reserved.
 //
 
 import UIKit
 
-class FeedViewController: UIViewController {
-
+class FeedsViewController: UIViewController {
+    fileprivate let tableViewCellReuseIdentifier = "FeedsCell"
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        self.tableView.backgroundColor = ThemeColor.white
+        self.view.backgroundColor = ThemeColor.white
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
-    */
-
 }
+
+extension FeedsViewController : UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellReuseIdentifier, for: indexPath) as! FeedsCell
+        return cell
+    }
+}
+
+// MARK: - TableViewDelegate Setup
+
+extension FeedsViewController : UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+}
+
+
