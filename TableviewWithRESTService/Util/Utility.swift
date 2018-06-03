c759a65f2d4d7f1228a96e70b278fc985c67313e
Nischal Hada <hadanischal@gmail.com>
2018-06-03 18:39:33 +1000

    added Utility

---
+++ TableviewWithRESTService/Util/Utility.swift
@@ -0,0 +1,24 @@
+//
+//  Utility.swift
+//  TableviewWithRESTService
+//
+//  Created by Nischal Hada on 5/31/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+protocol Utility{
+    func filterNil(_ value: AnyObject?) -> AnyObject?
+}
+
+struct Util {
+    
+}
+
+extension Util: Utility{
+    func filterNil(_ value: AnyObject?) -> AnyObject? {
+        return ((value is NSNull || value == nil) ?  "N/A" as AnyObject :  value)
+    }
+}
+
