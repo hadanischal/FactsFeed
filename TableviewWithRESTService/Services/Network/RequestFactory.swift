74b14322f908558bb1ff17bcd3ae9d875e2aff40
Nischal Hada <hadanischal@gmail.com>
2018-06-03 18:45:50 +1000

    added NetworkService

---
+++ TableviewWithRESTService/Services/Network/RequestFactory.swift
@@ -0,0 +1,22 @@
+//
+//  RequestFactory.swift
+//  TableviewWithRESTService
+//
+//  Created by Nischal Hada on 5/31/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+final class RequestFactory {
+    enum Method: String {
+        case GET,POST,PUT,DELETE,PATCH
+    }
+    
+    static func request(method: Method, url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        return request
+    }
+}
+
---
+++ TableviewWithRESTService/Services/Result/ErrorResult.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorResult.swift
+//  TableviewWithRESTService
+//
+//  Created by Nischal Hada on 5/31/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorResult: Error {
+    case network(string: String)
+    case parser(string: String)
+    case custom(string: String)
+}
+
