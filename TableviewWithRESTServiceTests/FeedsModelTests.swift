a1daac3bb8db3f8d1929d106ecd70266d33de34b
Nischal Hada <hadanischal@gmail.com>
2018-06-03 22:02:36 +1000

    work done on FeedsModelTests

--- TableviewWithRESTServiceTests/FeedsModelTests.swift
+++ TableviewWithRESTServiceTests/FeedsModelTests.swift
@@ -7,29 +7,71 @@
 //
 
 import XCTest
+@testable import TableviewWithRESTService
 
 class FeedsModelTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testExampleEmptyCurrency() {
+        let data = Data()
+        let completion : ((Result<FeedsModel, ErrorResult>) -> Void) = { result in
+            switch result {
+            case .success(_):
+                XCTAssert(false, "Expected failure when no data")
+            default:
+                break
+            }
+        }
+        ParserHelper.parse(data: data, completion: completion)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testParseCurrency() {
+        guard let data = FileManager.readJson(forResource: "facts") else {
+            XCTAssert(false, "Can't get data from sample.json")
+            return
+        }
+        let completion : ((Result<FeedsModel, ErrorResult>) -> Void) = { result in
+            switch result {
+            case .failure(_):
+                XCTAssert(false, "Expected valid converter")
+            case .success(let converter):
+                XCTAssertEqual(converter.title, "About Canada", "Expected About Canada base")
+                XCTAssertEqual(converter.rows.count, 14, "Expected 14 rates")
+            }
+        }
+        ParserHelper.parse(data: data, completion: completion)
+    }
+    
+    func testWrongKeyCurrency() {
+        let dictionary = ["testObject" : 123 as AnyObject]
+        let result = FeedsModel.parseObject(dictionary: dictionary)
+        switch result {
+        case .success(_):
+            XCTAssert(false, "Expected failure when wrong data")
+        default:
+            return
         }
     }
-    
 }
+extension FileManager {
+    static func readJson(forResource fileName: String ) -> Data? {
+        let bundle = Bundle(for: FeedsDataSourceTests.self)
+        if let path = bundle.path(forResource: fileName, ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                return data
+            } catch {
+            }
+        }
+        return nil
+    }
+}
+
+
